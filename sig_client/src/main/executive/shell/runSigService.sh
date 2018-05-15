e5329e0f1e5629daa7b86f8d85dc81bc55fd2415
cyjseagull <1547158861@qq.com>
2018-05-15 16:44:13 +0800

    Update runSigService.sh

--- sig_client/src/main/executive/shell/runSigService.sh
+++ sig_client/src/main/executive/shell/runSigService.sh
@@ -437,6 +437,7 @@ function update_group_sig_data()
 #join_group "conn.json" "group4" "member1"
 #group_sig "conn.json" "group4" "member1" "hello"
 #deploy_group_sig "conn.json" "group4" "member1" "hello"
+#true means that verify succ
 #eth_group_verify "conn.json" "0x1af99bf7d62bf6629459d47399358b23c19fcbd2"
 #update_group_sig_data "conn.json" "0x1af99bf7d62bf6629459d47399358b23c19fcbd2" "group4" "test1" "hello" 
 
@@ -448,6 +449,7 @@ function update_group_sig_data()
 #join_ring "conn.json" "ring1"
 #join_ring "conn.json" "ring1"
 #ring_sig "conn.json" "ring1" "0" "hello" "4"
+#1 represents that verify succ
 #deploy_ring_sig "conn.json" "hello" "ring1" "0" "4"
 #eth_ring_verify "conn.json" "0x418d9da6382993e6955135db364c7dba88e483a4"
 #update_ring_sig_data "conn.json" "0x418d9da6382993e6955135db364c7dba88e483a4" "hello" "ring1" "1" "4"
